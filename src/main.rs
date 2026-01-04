cd4ea2212c9d6295eecff37b8c8e7023baf5734c
Ishpreet Singh <ishpreetsingh8264@gmail.com>
2026-01-05 01:22:12 +0530

    Question 6, Print all Divisors of a given Number

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,20 @@
 fn main() {
-    let number = 153;
-    if amrstrong_number(number){
-        println!("{} is an Armstrong number", number);
-    }else{
-        println!("{} is not an Armstrong number", number);
-    }
+    println!("Hello World");
+    let n = 16;
+    let result: i32= divisor_count(n);
+    println!("Result is: {}  :)))))",result)
+
 }
 
-fn amrstrong_number(num: i32)-> bool{
-    let mut sum = 0;
-    let mut temp = num;
-    let digits = num.to_string().len() as u32;
-    while temp != 0{
-        let rem = temp %10;
-        sum += rem.pow(digits);
-        temp /=10;
+fn divisor_count(n: i32) -> i32 {
+    
+    let mut count: i32 =0;
+    let half:i32 = n /2;
+    
+    for i in 1..=half{
+        if n % i == 0{
+            count +=1;
+        }
     }
-    return sum == num;
+    return count
 }
