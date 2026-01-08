efc63c92d6d7fc12c1f28b50e8aa691b702daa81
Ishpreet Singh <ishpreetsingh8264@gmail.com>
2026-01-09 00:43:05 +0530

    basic hashing, Question 3,Find the highest/lowest frequency element

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,6 @@ use std::collections::HashMap;
 fn main() {
     let arr = [1, 2, 2, 3, 4, 4, 4, 5];
     count_frequency(&arr);
-
 }
 
 fn count_frequency(arr: &[i32]) {
@@ -13,7 +12,18 @@ fn count_frequency(arr: &[i32]) {
         *freq_map.entry(num).or_insert(0) += 1;
     }
 
-    for (key, value) in &freq_map {
-        println!("{}: {}", key, value);
+    let mut highest_freq = (0, 0); 
+    let mut lowest_freq = (0, i32::MAX);
+
+    for (&key, &value) in &freq_map {
+        if value > highest_freq.1 {
+            highest_freq = (key, value);
+        }
+        if value < lowest_freq.1 {
+            lowest_freq = (key, value);
+        }
     }
+
+    println!("Highest frequency element: {} with frequency {}", highest_freq.0, highest_freq.1);
+    println!("Lowest frequency element: {} with frequency {}", lowest_freq.0, lowest_freq.1);
 }
