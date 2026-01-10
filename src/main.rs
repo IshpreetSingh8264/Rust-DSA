53889a8bedc4a9a572fc424f1381b04f344e9748
Ishpreet Singh <ishpreetsingh8264@gmail.com>
2026-01-11 01:32:19 +0530

    Array Easy, Question 2, Find Second Smallest and Second Largest Element in an array but done using sorting

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 fn main() {
-    println!("Hello, dsaprep!");
-    let vector = vec![7, 2, 5, 3, 9, 1, 4];
+    let vector = vec![1,1,2,3,4,5,6,7,8,10,10];
     let result = find_second_smallest_largest_using_sorting(&vector);
     println!("Second Smallest: {}, Second Largest: {}", result[0], result[1])
 
@@ -9,6 +8,7 @@ fn main() {
 fn find_second_smallest_largest_using_sorting(arr: &Vec<i32>) -> [i32; 2]{
     let mut sorted_array = arr.clone();
     sorted_array.sort_unstable();
+    sorted_array.dedup();
     let second_smallest = sorted_array[1];
     let second_largest = sorted_array[sorted_array.len() - 2];
     [second_smallest, second_largest]
