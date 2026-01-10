c6d43ef809a315b202c83a2ee87c47c9da40fbfa
Ishpreet Singh <ishpreetsingh8264@gmail.com>
2026-01-11 01:06:38 +0530

    Array Easy, Question 1, Find the Largest element in an array

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,18 @@
-use std::collections::HashMap;
-
 fn main() {
-    let arr = [1, 2, 2, 3, 4, 4, 4, 5];
-    count_frequency(&arr);
+    let numbers = vec![34, 50, 25, 100, 65];
+
+    let result = largest_element(&numbers);
+    println!("The largest element is {}", result);
+
 }
 
-fn count_frequency(arr: &[i32]) {
-    let mut freq_map = HashMap::new();
 
-    for &num in arr {
-        *freq_map.entry(num).or_insert(0) += 1;
-    }
-
-    let mut highest_freq = (0, 0); 
-    let mut lowest_freq = (0, i32::MAX);
-
-    for (&key, &value) in &freq_map {
-        if value > highest_freq.1 {
-            highest_freq = (key, value);
-        }
-        if value < lowest_freq.1 {
-            lowest_freq = (key, value);
+fn largest_element(arr: &[i32]) -> i32 {
+    let mut largest = arr[0];
+    for &item in arr.iter() {
+        if item > largest {
+            largest = item;
         }
     }
-
-    println!("Highest frequency element: {} with frequency {}", highest_freq.0, highest_freq.1);
-    println!("Lowest frequency element: {} with frequency {}", lowest_freq.0, lowest_freq.1);
+    largest
 }
